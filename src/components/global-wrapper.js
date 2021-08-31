47460361504b65e8d67b364d3c94c1afd73e727d
Jerrick Hoang <jerrickhoang@gmail.com>
2021-08-30 22:45:03 -0400

    Fix

--- src/components/global-wrapper.js
+++ src/components/global-wrapper.js
@@ -21,40 +21,16 @@ const GlobalWrapper = (props) => {
     <Fragment>
       <Helmet>
         <html lang="en" />
-        <title>Gabriel Adorf</title>
-        <meta name="description" content="Gabriel Adorf's projects" />
+        <title>Jerrick Hoang</title>
+        <meta name="description" content="Jerrick Hoang's projects" />
         <meta
           name="keywords"
-          content="design, ux, ui, product, graphic, health, climbing, app, interface, experience, festival, techno, code"
+          content="robotics, machine learning, autonomous, vehicles, cruise, waymo, cmu, uber, self driving, tesla, autopilot"
         />
-        <meta
-          property="og:image"
-          content="https://www.gabrieladorf.com/static/kellerkindCover-14f87246d1614ff1cf339940bdc58186.jpg"
-        />
-        <meta property="og:description" content="Gabriel Adorf's projects" />
+        <meta property="og:description" content="Jerrick Hoang's projects" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://gabrieladorf.com" />
-        <meta property="og:title" content="Gabriel Adorf" />
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="theme-color" content="#ffffff" />
       </Helmet>
       <GlobalStyle displayOutlines={displayOutlines} />
